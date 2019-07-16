1947bd9967563482297e8d66b36b5cfc41f9eb41
owaisj <jamil.owais@gmail.com>
2019-07-15 23:04:03 -0500

    POST method for notes and updating

--- view/src/pages/Notes.js
+++ view/src/pages/Notes.js
@@ -5,16 +5,43 @@ class Notes extends Component {
         super(props);
         this.state = {
             article: {},
-            loading: true
+            loading: true,
+            notes: false
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    componentDidMount() {
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const bodyData = {
+            title: data.get('title'),
+            body: data.get('body')
+        }
+        fetch(`/api/articles/${this.props.match.params.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(bodyData)
+        }).then(res => res.json())
+        .then(data => this.grabArticle());
+    }
+    grabArticle() {
         fetch(`/api/articles/${this.props.match.params.id}`).then(res=>res.json())
         .then(data=>{
-            console.log(data);
-            return this.setState({article: data, loading: false})
+            if (data.note) {
+                return this.setState({article: data, loading: false, notes: true})
+            } else {
+                return this.setState({article: data, loading: false})
+            }
         })
+    }
+    componentDidMount() {
+        this.grabArticle();
     };
+    componentDidUpdate() {
+        this.grabArticle();
+    }
     render() {
         const content = this.state.article;
         return (
@@ -31,7 +58,38 @@ class Notes extends Component {
                 <div>
                     <img src={content.image} alt="From KUT" />
                 </div>
-                <form></form>
+
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: '75%',
+                    padding: '1em'
+                }}>
+                    <form style={{ margin: '1em'}} onSubmit={this.handleSubmit}>
+                        <p>
+                            Title
+                        </p>
+                        <input id="title" name="title" />
+                        <p>
+                            Message
+                        </p>
+                        <input id="body" name="body" />
+                        <br />
+                        <button>Update Note</button>
+                    </form>
+                    {this.state.notes ? 
+                        <div style={{ margin: '1em'}} >
+                            <h1>
+                                {content.note.title}
+                            </h1>
+                            <p>
+                                {content.note.body}
+                            </p>
+                        </div>
+                    : <div style={{ margin: '1em'}}>There are no notes!</div>}
+                </div>
             </div>
         )
     }
